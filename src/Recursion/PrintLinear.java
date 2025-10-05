b265ba10b4731b9845a8c5d88c639214decd9ec1
lumishka <mkuk3592@gmail.com>
2025-10-05 21:51:19 +0530

    Print 1 to N using recursion

---
+++ src/Recursion/PrintLinear.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PrintLinear {
+
+    static void print(int i, int n){
+        if(i>n){
+            return;
+        }
+        System.out.println(i);
+        print(i+1,n);
+
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        print(1,n);
+    }
+}
